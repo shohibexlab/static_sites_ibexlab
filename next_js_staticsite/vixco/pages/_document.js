9ca14f5b85766e9e746742154d65a79f56cbc694
shoh <komiljonov@ibexlab.com>
2022-02-09 18:50:41 +0900

    Nexts js static sites added

---
+++ next_js_staticsite/vixco/pages/_document.js
@@ -0,0 +1,28 @@
+
+    import Document, { Html, Head, Main, NextScript } from 'next/document'
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="shortcut icon"
+      href="/favicon.png"
+      type="image/x-icon"
+    />
+    {/* Title Name can be changed through here */}
+    <title>Vixco</title>
+        </Head>
+        <body style={{"overflowX": "hidden","overflowY": "hidden"}}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ next_js_staticsite/vixco/pages/index.js
@@ -0,0 +1,14 @@
+export default function STATIC() {
+  return (
+    <div style={{height: "100vh"}}>
+      <iframe src="https://shohibexlab.github.io/static_sites_ibexlab/vixco_net"
+             frameBorder="0" 
+             width="100%" 
+             height="100%"
+             scrolling="none"
+             ></iframe>
+             </div>
+  )
+}
+
+
