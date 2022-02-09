458cc855117a4b6ca94fabad71342431ea1e4227
shoh <komiljonov@ibexlab.com>
2022-02-09 18:37:35 +0900

    Update

---
+++ vixco_net/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>http://vixco.net</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://vixco.net/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://vixco.net/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://vixco.net/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://vixco.net/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://vixco.net/index.php?rest_route=/" />
+			</apis>
+	</service>
+</rsd>
+	
