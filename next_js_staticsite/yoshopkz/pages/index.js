464d12730b1434b46fce7cef4a5fb269802607d8
Nishat Nasir <nishat@ibexlab.com>
2022-02-11 15:53:14 +0900

    Link updated

--- next_js_staticsite/yoshopkz/pages/index.js
+++ next_js_staticsite/yoshopkz/pages/index.js
@@ -1,10 +1,12 @@
 
 
 export default function STATIC() {
+  var localLink = "../../yoshop_kz/www.yoshop.kz/ru/index.html"
+  var globalLink = "https://shohibexlab.github.io/static_sites_ibexlab/yoshop_kz/www.yoshop.kz/ru/"
   return (
     <div style={{height: "100vh"}}>
      
-        <iframe src="https://shohibexlab.github.io/static_sites_ibexlab/yoshop_kz/www.yoshop.kz/ru/"
+        <iframe src={globalLink}
               // https://shohibexlab.github.io/static_sites_ibexlab/yoshop_kz/www.yoshop.kz/ru/
               // ../../yoshop_kz/www.yoshop.kz/ru/index.html
              frameBorder="0" 
