a540a5a211452e6a88d52ba9a5120dbb9f59e493
shoh <komiljonov@ibexlab.com>
2022-02-11 10:46:39 +0900

    icons fix for vixco

--- next_js_staticsite/vixco/pages/index.js
+++ next_js_staticsite/vixco/pages/index.js
@@ -2,6 +2,7 @@ export default function STATIC() {
   return (
     <div style={{height: "100vh"}}>
       <iframe src="https://shohibexlab.github.io/static_sites_ibexlab/vixco_net"
+      // <iframe src="../../vixco.net/index.html"
              frameBorder="0" 
              width="100%" 
              height="100%"
