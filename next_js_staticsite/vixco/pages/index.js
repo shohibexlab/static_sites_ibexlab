5a44cfa4d3ab8cc518089ec359fd2bce98a7c9ef
shoh <komiljonov@ibexlab.com>
2022-02-11 11:00:05 +0900

    vixco update

--- next_js_staticsite/vixco/pages/index.js
+++ next_js_staticsite/vixco/pages/index.js
@@ -2,7 +2,7 @@ export default function STATIC() {
   return (
     <div style={{height: "100vh"}}>
       <iframe src="https://shohibexlab.github.io/static_sites_ibexlab/vixco_net"
-      // <iframe src="../../vixco.net/index.html"
+      // <iframe src="../../vixco_net/index.html"
              frameBorder="0" 
              width="100%" 
              height="100%"
